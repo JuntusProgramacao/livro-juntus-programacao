807c0714aa4853b81e9ce488bde52b1257031d6e
Juntus Programação <144743726+JuntusProgramacao@users.noreply.github.com>
2024-01-31 21:27:59 -0300

    Delete

--- 4 - Estrutura de Repetição/Exercicio.14.py
+++
@@ -1,8 +0,0 @@
-# Faça um algoritmo para imprimir os números pares entre 8 e 18.
-
-print('Esse programa vai imprimir os números pares entre 8 e 18')
-
-for numero in range(8,18 +1):
- if numero %2 == 0:
-  print(numero)
-
