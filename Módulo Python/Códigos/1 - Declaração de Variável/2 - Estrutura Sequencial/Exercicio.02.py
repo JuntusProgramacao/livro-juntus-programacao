a64701bb47beb319c778c4fd9cc94e44c96c665b
Juntus Programação <144743726+JuntusProgramacao@users.noreply.github.com>
2024-01-31 21:31:40 -0300

    Delete

--- Módulo Python/Códigos/1 - Declaração de Variável/2 - Estrutura Sequencial/Exercicio.02.py
+++
@@ -1,9 +0,0 @@
-#2. Faça um algoritmo para ler quatro notas e imprimir a média ponderada
-# dessas notas. Considere que os pesos das são: 1, 2, 3 e 4, respectivamente.
-
-nota1 = float(input('Digite sua nota 1: '))
-nota2 = float(input('Digite sua nota 2: '))
-nota3 = float(input('Digite sua nota 3: '))
-nota4 = float(input('Digite sua nota 4: '))
-print('Sua nota é:', (nota1*1 + nota2*2 + nota3*3 + nota4*4) / 10)
-
