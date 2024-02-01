6064fa165e85076f8b27fd014f87e586c5556f15
Juntus Programação <144743726+JuntusProgramacao@users.noreply.github.com>
2024-01-31 21:27:48 -0300

    Delete

--- 3 - Estrutura de Decisão/Exercicio.08.py
+++
@@ -1,8 +0,0 @@
-#8. Leia um número e imprima se ele é par ou ímpar.
-
-numero = int(input("Digite um número: "))
-
-if numero % 2 == 0:
-    print("O número é par.")
-else:
-    print("O número é ímpar.")
