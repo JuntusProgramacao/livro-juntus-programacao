23f1e127e2e219596a845d9d0c03e7e8039c2c5f
Juntus Programação <144743726+JuntusProgramacao@users.noreply.github.com>
2024-03-04 12:06:47 -0300

    Add files via upload

---
+++ Livro - Módulo C#/1 - declaracao-de-variaveis/Exercicio.01/Program.cs
@@ -0,0 +1,32 @@
+﻿// Identifique e declare as variáveis presentes (grifadas) nas frases abaixo: 
+
+// a) Ana Maria é mãe de cinco filhos. Ela caminha todos os dias 10 km para levá-los à escola “EEPSG Josuá da Silva”
+string nome = "Ana Maria";
+string numeroDeFilhos = "cinco"; //##########################
+double distancia = 10;
+string nomeDaEscola = "EEPSG Josuá da Silva";
+
+// b) Sr. Felisberto representou o Brasil nas Olimpíadas de Barcelona em 1982. Ele ganhou 3 medalhas no nado livre.
+int ano = 1982;
+int numeroDeMedalhas = 3;
+string estilo = "nado livre";
+
+// c) José comeu meia maçã. Ele precisou subir cinco degraus para alcançá-la.
+string proporcaoMaca = "meia"; //##########################
+string numeroDeDegraus = "cinco";
+
+// d) A fórmula para calcular a área do círculo é PI * (RAIO * RAIO).
+double area;
+double equacao = PI * (RAIO * RAIO); //##########################
+
+// e) José ganhou cinco carros no sorteio “Ajuda de amigos!”.
+string quantidadeDoItem = "cinco";
+string nomeDoItem = "carros";
+
+// f) O salário de Pedro mal dá para comprar alguns quilos de carne.
+double salario;
+string artigo = "quilos de carne";
+
+// g) Maria é muito tímida, ela mal consegue falar a frase: “Eu quero uma passagem para São Paulo”. Ela tem três irmãos e dois primos.
+string frase = "Eu quero uma passagem para São Paulo";
+string numeroDeParentes = "três irmãos e dois primos";
---
+++ Livro - Módulo C#/1 - declaracao-de-variaveis/Exercicio.02a/Program.cs
@@ -0,0 +1,35 @@
+﻿// Calcule o valor final das variáveis X, Y, Z, A e K sabendo que as atribuições abaixo representam um bloco de comandos de um programa. Faça a declaração das variáveis X, Y, Z, A e K na linguagem desejada.
+
+// a) X ← 10;
+// Y ← 15;
+// Z ← 32;
+// X ← X + Y;
+// Y ← Z – X;
+// A ← 25;
+// Z ← A + 14 MOD 3;
+// K ← 0;
+// K ← K + 1;
+// K ← K + 1;
+// K ← K + 1;
+// K ← K + A;
+
+int X, Y, Z, A, K;
+
+X = 10;
+Y = 15;
+Z = 32;
+X += Y;
+Y = Z - X;
+A = 25;
+Z = A + (14 % 3);
+K = 0;
+K++;
+K++;
+K++;
+K += A;
+
+Console.WriteLine($"X = {X}");
+Console.WriteLine($"Y = {Y}");
+Console.WriteLine($"Z = {Z}");
+Console.WriteLine($"A = {A}");
+Console.WriteLine($"K = {K}");
