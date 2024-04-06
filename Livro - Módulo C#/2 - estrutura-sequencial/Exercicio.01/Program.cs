df37b5c5f7c9c67fed3760dbee3937065322e6e0
Juntus Programação <144743726+JuntusProgramacao@users.noreply.github.com>
2024-04-05 21:01:34 -0300

    Add files via upload

---
+++ Livro - Módulo C#/2 - estrutura-sequencial/Exercicio.01/Program.cs
@@ -0,0 +1,19 @@
+﻿//1. Faça um algoritmo para ler um número, somá-lo com o valor 10 e mostrar a média entre o número lido e o valor 10.
+
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Digite um número inteiro: "); // imprime na tela a mensagem para o usuário ler.
+        double num = Convert.ToInt32(Console.ReadLine()); // essa linha de comando armazena o dado inserido pelo usuário.
+
+        double soma = num + 10; // realização da soma
+        double media = soma / 2; //cálculo da média
+
+        Console.WriteLine($"A média de {soma} é igual a {media}."); //exibe a resposta do enunciado fazendo o uso da interpolação.
+    }
+}
+
+
---
+++ Livro - Módulo C#/2 - estrutura-sequencial/Exercicio.02/Program.cs
@@ -0,0 +1,31 @@
+﻿//2. Faça um algoritmo para ler quatro notas e imprimir a média ponderada dessas notas. 
+// Considere que os pesos das são: 1, 2, 3 e 4, respectivamente.
+
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Digite a primeira nota: ");
+        double nota1 = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Digite a segunda nota: ");
+        double nota2 = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Digite a terceira nota: ");
+        double nota3 = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Digite a quarta nota: ");
+        double nota4 = Convert.ToDouble(Console.ReadLine());
+
+        int peso1 = 1;
+        int peso2 = 2;
+        int peso3 = 3;
+        int peso4 = 4;
+
+        double mediaPonderada = ((nota1 * peso1) + (nota2 * peso2) + (nota3 * peso3) + (nota4 * peso4))
+                / (peso1 + peso2 + peso3 + peso4);
+
+        Console.WriteLine($"A média ponderada das notas é igual a {mediaPonderada:F1}");
+    }
+}
+
+
