bdb976941f6a0f2073d73f5c24bdcc76c9001aef
Juntus Programação <144743726+JuntusProgramacao@users.noreply.github.com>
2024-04-05 21:52:28 -0300

    Add files via upload

---
+++ Livro - Módulo C#/2 - estrutura-sequencial/Exercicio.24/Program.cs
@@ -0,0 +1,30 @@
+﻿//24. Faça um programa que leia os valores A, B, C, D, E, F e encontre o valor de X
+// X = (a + (b/c) / d - 2 * (e/f)) + 4a
+
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Digite o valor de A: ");
+        double a = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Digite o valor de B: ");
+        double b = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Digite o valor de C: ");
+        double c = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Digite o valor de D: ");
+        double d = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Digite o valor de E: ");
+        double e = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Digite o valor de F: ");
+        double f = Convert.ToDouble(Console.ReadLine());
+
+        double x = (a + (b / c)) / (((d - 2) * (e / f)) + (4 * a));
+
+        Console.WriteLine($"O valor de x é igual a {x:F2}.");
+    }
+}
+
+
+
---
+++ Livro - Módulo C#/2 - estrutura-sequencial/Exercicio.25/Program.cs
@@ -0,0 +1,24 @@
+﻿//25. Faça um algoritmo que leia o valor dos catetos de um triângulo retângulo, calcule e mostre sua hipotenusa.
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Leitura dos catetos
+        Console.Write("Digite o valor do primeiro cateto: ");
+        double cateto1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Digite o valor do segundo cateto: ");
+        double cateto2 = Convert.ToDouble(Console.ReadLine());
+
+        // Cálculo da hipotenusa
+        double hipotenusa = Math.Sqrt(Math.Pow(cateto1, 2) + Math.Pow(cateto2, 2));
+
+        // Exibição do resultado
+        Console.WriteLine($"O valor da hipotenusa é igual a {hipotenusa:F2}.");
+    }
+}
+
+
