729825ab0611c4e4f159f18008f610a4c32a2032
Juntus Programação <144743726+JuntusProgramacao@users.noreply.github.com>
2024-04-05 21:55:26 -0300

    Add files via upload

---
+++ Livro - Módulo C#/2 - estrutura-sequencial/Exercicio.28/Program.cs
@@ -0,0 +1,24 @@
+﻿//28. Um funcionário recebe um salário fixo mais 4,0% de comissão sobre as vendas.
+// Faça um algoritmo que receba o salário fixo e o valor das vendas,
+// calcule e mostre a comissão e o salário final do funcionário.
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Digite o salário fixo do funcionário em R$: ");
+        double salarioFixo = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Digite o valor das vendas em R$: ");
+        double valorVendas = Convert.ToDouble(Console.ReadLine());
+
+        double comissao = valorVendas * 0.04;
+        double salarioFinal = salarioFixo + comissao;
+
+        Console.WriteLine($"A comissão é de R$ {comissao}.");
+        Console.WriteLine($"O salário final é R$ {salarioFinal}.");
+    }
+}
+
+
---
+++ Livro - Módulo C#/2 - estrutura-sequencial/Exercicio.29/Program.cs
@@ -0,0 +1,31 @@
+﻿//29. Faça um algoritmo que leia o ano de nascimento de uma pessoa e o ano atual, calcule e mostre:
+// - A idade dessa pessoa em anos;
+// - A idade dessa pessoa em meses;
+// - A idade dessa pessoa em dias;
+// - A idade dessa pessoa em semanas.
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Digite o ano de nascimento: ");
+        int anoNascimento = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Digite o ano atual: ");
+        int anoAtual = Convert.ToInt32(Console.ReadLine());
+
+        int idadeAnos = anoAtual - anoNascimento;
+        int idadeMeses = idadeAnos * 12;
+        int idadeDias = idadeAnos * 365;
+        int idadeSemanas = idadeDias / 7;
+
+        Console.WriteLine("Idade em anos: " + idadeAnos);
+        Console.WriteLine("Idade em meses: " + idadeMeses);
+        Console.WriteLine("Idade em dias: " + idadeDias);
+        Console.WriteLine("Idade em semanas: " + idadeSemanas);
+    }
+}
+
+
+
