db1434fdd263cad8212b2fc56b1a883c2c1e9682
Juntus Programação <144743726+JuntusProgramacao@users.noreply.github.com>
2024-04-05 21:21:02 -0300

    Add files via upload

---
+++ Livro - Módulo C#/2 - estrutura-sequencial/Exercicio.11/Program.cs
@@ -0,0 +1,26 @@
+﻿// 11.	Desenvolva um algoritmo que:
+// • Peca o ano de nascimento.
+// • Calcule e mostre a idade em dias.
+
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Digite o ano do seu nascimento: ");
+        int anoNascimento = Convert.ToInt32(Console.ReadLine());
+        int anoCorrente = DateTime.Now.Year;
+        // a estrutura DateTime representa um momento no tempo, geralmente expresso como uma data ou hora do dia. 
+        // Na expressão desse caso, a estrutura manipula e exibe o ano atual.
+        int calculo = (anoCorrente - anoNascimento) * 365;
+
+        Console.WriteLine(
+            $"Você nasceu em {anoNascimento} e tem aproximadamente {calculo} dias de vida."
+        );
+    }
+}
+
+
+
+
---
+++ Livro - Módulo C#/2 - estrutura-sequencial/Exercicio.12/Program.cs
@@ -0,0 +1,25 @@
+﻿//12. Desenvolva um algoritmo que:
+// •	Peca o valor do produto
+// •	Peca a valor da aliquota
+// •	Calcule o valor em reais da porcentagem informada.
+
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Informe o valor do produto em reais: ");
+        double valorProduto = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Informe quantos porcento de desconto o cliente pode receber: ");
+        double valorAliquota = Convert.ToDouble(Console.ReadLine());
+
+        double calculo = valorProduto * (valorAliquota / 100);
+
+        Console.WriteLine(
+            $"O produto de R$ {valorProduto:F2}, com o desconto de {valorAliquota}%, subtrai-se R$ {calculo:F2} do seu valor total."
+        );
+    }
+}
+
+
