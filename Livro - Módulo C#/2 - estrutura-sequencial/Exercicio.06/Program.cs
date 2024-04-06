f07354b64b95978892b24ebd983f49387bec3f1b
Juntus Programação <144743726+JuntusProgramacao@users.noreply.github.com>
2024-04-05 21:07:44 -0300

    Add files via upload

---
+++ Livro - Módulo C#/2 - estrutura-sequencial/Exercicio.06/Program.cs
@@ -0,0 +1,24 @@
+﻿//6. Faça um algoritmo para ler dois números e realizar as operações aritméticas: adição, subtração e multiplicação.
+// Para cada operação o resultado deve ser impresso no vídeo.
+
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Digite o primeiro número: ");
+        double num1 = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Digite o segundo número: ");
+        double num2 = Convert.ToDouble(Console.ReadLine());
+        //As operações podem ser feitas fora da declaração do resultado, em variáveis a serem exibidas.
+        //Exemplo:
+        //double soma = num1 + num2;
+
+        Console.WriteLine($"Adição: {num1} + {num2} = {num1 + num2}");
+        Console.WriteLine($"Subtração: {num1} - {num2} = {num1 - num2}");
+        Console.WriteLine($"Multiplicação: {num1} x {num2} = {num1 * num2}");
+    }
+}
+
+
---
+++ Livro - Módulo C#/2 - estrutura-sequencial/Exercicio.07/Program.cs
@@ -0,0 +1,20 @@
+﻿// 7. Faça um algoritmo que leia um número inteiro e imprima seu dobro e sua metade.
+
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Digite um número inteiro: ");
+        float numero = float.Parse(Console.ReadLine());
+
+        float dobro = numero * 2;
+        float metade = numero / 2;
+
+        Console.WriteLine(
+            $"O dobro de {numero} é igual a {dobro}. A metade de {numero} é igual a {metade}."
+        );
+    }
+}
+
