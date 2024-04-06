729c3db04e318e651e1986c4a651bfd96c2c68bd
Juntus Programação <144743726+JuntusProgramacao@users.noreply.github.com>
2024-04-05 21:57:32 -0300

    Add files via upload

---
+++ Livro - Módulo C#/3 - estrutura-de-decisao/Exercicio.01/Program.cs
@@ -0,0 +1,31 @@
+﻿// 7.	Desenvolva um programa para resolver as seguintes equações:
+// • x*y
+// • x²+y (para fazer x2, use: x*x ou x^2)
+// • (4/r) * (w+r)
+
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Digite um valor para x: ");
+        int x = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Digite um valor para y: ");
+        int y = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Digite um valor para r: ");
+        int r = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Digite um valor para w: ");
+        int w = Convert.ToInt32(Console.ReadLine());
+
+        double equacao1 = x * y;
+        double equacao2 = (x * x) + y;
+        double equacao3 = (4 / r) * (w + r);
+
+        Console.WriteLine($"{x} x {y} = {equacao1}");
+        Console.WriteLine($"{x}² + {y} = {equacao2}");
+        Console.WriteLine($"(4/{r}) x ({w} + {r}) = {equacao3}");
+    }
+}
+
+
---
+++ Livro - Módulo C#/3 - estrutura-de-decisao/Exercicio.02/Program.cs
@@ -0,0 +1,32 @@
+﻿//2. A importância de R$ 780.000,00 será dividida entre os três primeiros colocados de um concurso,
+// em partes diretamente proporcional aos pontos conseguidos por eles. Construa um programa que leia o
+// numero de pontos dos três primeiros colocados e imprima a importância que caberá a cada um deles.
+
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Digite o número de pontos do primeiro colocado: ");
+        int pontosPrimeiroColocado = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Digite o número de pontos do segundo colocado: ");
+        int pontosSegundoColocado = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Digite o número de pontos do terceiro colocado: ");
+        int pontosTerceiroColocado = Convert.ToInt32(Console.ReadLine());
+
+        int totalPontos = pontosPrimeiroColocado + pontosSegundoColocado + pontosTerceiroColocado;
+
+        Console.WriteLine("Digite o valor total a ser dividido (em reais): ");
+        double valorTotal = Convert.ToDouble(Console.ReadLine());
+
+        double valorPrimeiroColocado = (pontosPrimeiroColocado * valorTotal) / totalPontos;
+        double valorSegundoColocado = (pontosSegundoColocado * valorTotal) / totalPontos;
+        double valorTerceiroColocado = (pontosTerceiroColocado * valorTotal) / totalPontos;
+
+        Console.WriteLine($"O valor para o primeiro colocado é de R$ {valorPrimeiroColocado:F2}.");
+        Console.WriteLine($"O valor para o segundo colocado é de R$ {valorSegundoColocado:F2}.");
+        Console.WriteLine($"O valor para o terceiro colocado é de R$ {valorTerceiroColocado:F2}.");
+    }
+}
+
