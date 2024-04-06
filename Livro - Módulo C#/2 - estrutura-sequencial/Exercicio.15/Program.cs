6fa109f7ec955deb397871a0fbd3198354a0bd9f
Juntus Programação <144743726+JuntusProgramacao@users.noreply.github.com>
2024-04-05 21:25:33 -0300

    Add files via upload

---
+++ Livro - Módulo C#/2 - estrutura-sequencial/Exercicio.15/Program.cs
@@ -0,0 +1,24 @@
+﻿//15. Desenvolva um algoritmo para calcular o montante resultante de um capital aplicado a juros compostos. Você deve pedir 
+// o capital (C), a taxa de juros (I) e o tempo (N). Com essas variáveis, você deve calcular o montante (M) pela fórmula:
+// • M < - C * (1+(I/100)) ^N
+
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Informe o capital inicial em reais (R$): ");
+        double capitalInicial = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Informe a taxa de juros (%): ");
+        double taxaJuros = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Informe em quantos meses a dívida deverá ser paga: ");
+        double tempoMeses = Convert.ToDouble(Console.ReadLine());
+
+        double montante = Math.Round(capitalInicial * Math.Pow(1 + (taxaJuros / 100), tempoMeses), 2);
+
+        Console.WriteLine($"O montante resultante do capital aplicado é igual a R$ {montante:F2}.");
+    }
+}
+
+
---
+++ Livro - Módulo C#/2 - estrutura-sequencial/Exercicio.16/Program.cs
@@ -0,0 +1,22 @@
+﻿//16. Escreva um algoritmo que solicite ao usuário a altura e o raio de um cilindro circular,
+// e imprima o volume do cilindro. O volume do cilindro circular é calculado pela seguinte formula:
+// volume = 3.141592 * raio * raio * altura.
+
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Informe a altura do cilindro em centímetros (cm): ");
+        double altura = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Informe o raio do cilindro em centímetros (cm): ");
+        double raio = Convert.ToDouble(Console.ReadLine());
+        double volume = Math.Round(Math.PI * raio * raio * altura, 2);
+
+        Console.WriteLine($"O volume do cilindro é de {volume}cm³.");
+    }
+}
+
+
+
