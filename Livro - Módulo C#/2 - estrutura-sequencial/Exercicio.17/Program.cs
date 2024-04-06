be83b62c5443d77a5e80837c2204775a2d4e072b
Juntus Programação <144743726+JuntusProgramacao@users.noreply.github.com>
2024-04-05 21:29:18 -0300

    Add files via upload

---
+++ Livro - Módulo C#/2 - estrutura-sequencial/Exercicio.17/Program.cs
@@ -0,0 +1,20 @@
+﻿//17. Faça o cálculo da área de um retângulo a partir dos valores de base e de altura introduzidos pelo usuário
+
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Informe a altura do retângulo em centímetros (cm): ");
+        double altura = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Informe a base do retângulo em centímetros (cm): ");
+        double basee = Convert.ToDouble(Console.ReadLine());
+
+        double area = basee * altura;
+
+        Console.WriteLine($"A área do retângulo é de {area}cm².");
+    }
+}
+
+
---
+++ Livro - Módulo C#/2 - estrutura-sequencial/Exercicio.18/Program.cs
@@ -0,0 +1,34 @@
+﻿//18. Você foi encarregado de desenvolver o modulo de cálculo da folha de pagamento de uma empresa.
+// O funcionamento do programa deve ser o seguinte:
+// •	Peça o nome do empregado;
+// •	Peça o salário bruto;
+// •	Peça o valor do Imposto de Renda em reais que será descontado;
+// •	Peça o valor da previdencia (INSS) em reais que será descontado;
+// •	Calcule o valor do salário liquido do empregado;
+// •	Informe o salário liquido com a seguinte frase:
+//       "Fulano, seu salário liquido é: ......"
+
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Informe o nome do empregado: ");
+        string nome = Console.ReadLine();
+
+        Console.WriteLine("Informe o salário bruto do empregado em reais (R$): ");
+        double salarioBruto = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Informe o valor do Imposto de Renda em reais (R$): ");
+        double impostoRenda = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Informe o valor da Previdência Social (INSS) em reais (R$): ");
+        double inss = Convert.ToDouble(Console.ReadLine());
+
+        double salarioLiquido = salarioBruto - impostoRenda - inss;
+
+        Console.WriteLine($"{nome}, seu salário líquido é de R$ {salarioLiquido:F2}.");
+    }
+}
+
