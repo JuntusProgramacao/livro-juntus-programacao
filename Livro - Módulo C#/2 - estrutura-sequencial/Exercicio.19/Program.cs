999062f80d431af40e312a0382a24b1526352736
Juntus Programação <144743726+JuntusProgramacao@users.noreply.github.com>
2024-04-05 21:31:35 -0300

    Add files via upload

---
+++ Livro - Módulo C#/2 - estrutura-sequencial/Exercicio.19/Program.cs
@@ -0,0 +1,24 @@
+﻿//19. Você foi contratado por uma empresa de construção para fazer um programa que calcule o salário liquido dos operários no fim de 
+// cada mês, sabe-se que cada operário recebe R$ 3,00 por cada hora trabalhada, e que se desconta 8% do salário bruto para INSS.
+
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Digite a quantidade de horas trabalhadas pelo operário no mês: ");
+        int horasTrabalhadas = Convert.ToInt32(Console.ReadLine());
+
+        double valorHora = 3.00;
+        double salarioBruto = horasTrabalhadas * valorHora;
+        double inss = salarioBruto * 0.08;
+        double salarioLiquido = salarioBruto - inss;
+
+        Console.WriteLine($"Salário bruto = R${salarioBruto:F2}");
+        Console.WriteLine($"Desconto INSS = R${inss:F2}");
+        Console.WriteLine($"Salário líquido = R${salarioLiquido:F2}");
+    }
+}
+
+
---
+++ Livro - Módulo C#/2 - estrutura-sequencial/Exercicio.20/Program.cs
@@ -0,0 +1,21 @@
+﻿//20. Você foi contratado por uma loja de eletrônicos para fazer um algoritmo que calcule a conversão de dólares para real,
+// sabe-se que o programa deverá ler a cotação do Dólar do dia e o valor a ser convertido.
+
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Digite a cotação do dólar (US$) do dia: ");
+        double cotacaoDolar = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Digite o valor em dólar (US$) a ser convertido: ");
+        double valorDolar = Convert.ToDouble(Console.ReadLine());
+
+        double valorReal = valorDolar * cotacaoDolar;
+
+        Console.WriteLine($"O valor em Real é de R$ {valorReal:F2}.");
+    }
+}
+
