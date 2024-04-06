411923f6ce7da961756317e4dbf964a968ce70ba
Juntus Programação <144743726+JuntusProgramacao@users.noreply.github.com>
2024-04-05 21:54:24 -0300

    Add files via upload

---
+++ Livro - Módulo C#/2 - estrutura-sequencial/Exercicio.26/Program.cs
@@ -0,0 +1,53 @@
+﻿//26. O proprietário de um restaurante deseja informatizar o seu estabelecimento,
+// para tanto você foi contratado com o seguinte propósito. Elabore um algoritmo que
+// leia o número da mesa e qual a quantidade dos itens consumidos de um determinado cardápio, 
+// para que se possa saber o valor total desta mesa (conta), o algoritmo deverá prever a entrada
+// do número de pessoas existentes na mesa para que seja feita a divisão da mesma, entre os seus ocupantes.
+// Cardápio:
+//-Refrigerante	    1.20
+//-Cerveja  	    1.50
+//-Almoço	        6.30
+//-Porção		    4.50
+//-Lanche		    4.00
+
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double precoRefrigerante = 1.20;
+        double precoCerveja = 1.50;
+        double precoAlmoco = 6.30;
+        double precoPorcao = 4.50;
+        double precoLanche = 4.00;
+
+        Console.WriteLine("Digite o número da mesa: ");
+        int numeroMesa = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Digite a quantidade de refrigerantes consumidos: ");
+        int quantidadeRefrigerante = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Digite a quantidade de cervejas consumidas: ");
+        int quantidadeCerveja = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Digite a quantidade de almoços consumidos: ");
+        int quantidadeAlmoco = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Digite a quantidade de porções consumidas: ");
+        int quantidadePorcao = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Digite a quantidade de lanches consumidos: ");
+        int quantidadeLanche = Convert.ToInt32(Console.ReadLine());
+
+        double totalConta =
+            (precoRefrigerante * quantidadeRefrigerante)
+            + (precoCerveja * quantidadeCerveja)
+            + (precoAlmoco * quantidadeAlmoco)
+            + (precoPorcao * quantidadePorcao)
+            + (precoLanche * quantidadeLanche);
+
+        Console.WriteLine("Digite o número de pessoas na mesa: ");
+        int numeroPessoas = Convert.ToInt32(Console.ReadLine());
+
+        double valorPorPessoa = totalConta / numeroPessoas;
+
+        Console.WriteLine($"Valor total da conta da mesa {numeroMesa} é de R$ {totalConta:F2}.");
+        Console.WriteLine($"Valor por pessoa = R$ {valorPorPessoa:F2}");
+    }
+}
---
+++ Livro - Módulo C#/2 - estrutura-sequencial/Exercicio.27/Program.cs
@@ -0,0 +1,62 @@
+﻿//27. A Empresa ABC produz 3 tipos de peças mecânicas: parafusos, porcas e arruelas. Sabe-se que é dado desconto de: 
+// 10% por porca vendida, 20% por Arruelas vendidas, 30% por parafusos vendidos.
+// O algoritmo deve receber os seguintes dados:
+// - nome do cliente; 
+// - preço unitário da porca, arruela, parafuso; 
+// - quantidade de porcas, arruelas e parafusos solicitados pelo cliente.
+// Por fim, deve-se imprimir os seguintes dados:
+// - nome do cliente; 
+// - número de porcas, arruelas e parafusos comprados; 
+// - total pago pelas porcas, arruelas e parafusos; 
+// - total de desconto dado ao cliente; 
+// - total a ser pago pelo cliente.
+
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Digite o nome do cliente: ");
+        string nomeCliente = Console.ReadLine();
+
+        Console.WriteLine("Digite o preço unitário da porca: ");
+        double precoPorca = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Digite o preço unitário da arruela: ");
+        double precoArruela = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Digite o preço unitário do parafuso: ");
+        double precoParafuso = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Digite a quantidade de porcas solicitadas: ");
+        int quantidadePorcas = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Digite a quantidade de arruelas solicitadas: ");
+        int quantidadeArruelas = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Digite a quantidade de parafusos solicitados: ");
+        int quantidadeParafusos = Convert.ToInt32(Console.ReadLine());
+
+        double totalSemDesconto =
+            (precoPorca * quantidadePorcas)
+            + (precoArruela * quantidadeArruelas)
+            + (precoParafuso * quantidadeParafusos);
+
+        double desconto =
+            (precoPorca * quantidadePorcas * 0.1)
+            + (precoArruela * quantidadeArruelas * 0.2)
+            + (precoParafuso * quantidadeParafusos * 0.3);
+
+        double totalAPagar = totalSemDesconto - desconto;
+
+        Console.WriteLine($"Nome do cliente: {nomeCliente}");
+        Console.WriteLine($"Quantidade de porcas compradas: {quantidadePorcas}");
+        Console.WriteLine($"Quantidade de arruelas compradas: {quantidadeArruelas}");
+        Console.WriteLine($"Quantidade de parafusos comprados: {quantidadeParafusos}");
+        Console.WriteLine($"Total pago pelas porcas, arruelas e parafusos: R$ {totalSemDesconto:F2}");
+        Console.WriteLine($"Total de desconto dado ao cliente: R$ {desconto:F2}");
+        Console.WriteLine($"Total a ser pago pelo cliente: R$ {totalAPagar:F2}");
+    }
+}
+
