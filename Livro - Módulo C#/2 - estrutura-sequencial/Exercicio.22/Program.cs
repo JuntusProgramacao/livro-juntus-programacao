141cabac092ffd2933a01c9a176debfbc0e35d8f
Juntus Programação <144743726+JuntusProgramacao@users.noreply.github.com>
2024-04-05 21:50:47 -0300

    Add files via upload

---
+++ Livro - Módulo C#/2 - estrutura-sequencial/Exercicio.22/Program.cs
@@ -0,0 +1,18 @@
+﻿//22. Durante uma viagem à Inglaterra você foi obrigado a comprar um termômetro, e verificou que este estava marcando
+// a temperatura em Fahrenheit. Faça um algoritmo para calcular a conversão de graus Celcius para Fahrenheit.
+
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Digite a temperatura em graus Celsius (°C): ");
+        double celsius = Convert.ToDouble(Console.ReadLine());
+
+        double fahrenheit = (celsius * 9 / 5) + 32;
+
+        Console.WriteLine($"Temperatura em Fahrenheit = {fahrenheit}°F");
+    }
+}
+
---
+++ Livro - Módulo C#/2 - estrutura-sequencial/Exercicio.23/Program.cs
@@ -0,0 +1,40 @@
+﻿//23. Um sistema de equações lineares do tipo: ax+by=c e dx+ey=f,pode ser resolvido segundo mostrado abaixo:
+// y = af-cd/ae-bd
+// X = ce-bf/ae-bd 
+//Escreva um algoritmo que lê os coeficientes a,b,c,d ,e e f e calcula e mostre valores X e Y.
+
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Digite o valor de a: ");
+        double a = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Digite o valor de b: ");
+        double b = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Digite o valor de c: ");
+        double c = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Digite o valor de d: ");
+        double d = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Digite o valor de e: ");
+        double e = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Digite o valor de f: ");
+        double f = Convert.ToDouble(Console.ReadLine());
+
+        double denominador = (a * e) - (b * d);
+
+        double x = ((c * e) - (b * f)) / denominador;
+        double y = ((a * f) - (c * d)) / denominador;
+
+        Console.WriteLine($"Valor de x: {x}");
+        Console.WriteLine($"Valor de y: {y}");
+    }
+}
+
+
