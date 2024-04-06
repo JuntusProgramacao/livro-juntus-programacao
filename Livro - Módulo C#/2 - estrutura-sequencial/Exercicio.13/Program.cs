96ad373591f8e9157c17abafa70ad44ff2ab3891
Juntus Programação <144743726+JuntusProgramacao@users.noreply.github.com>
2024-04-05 21:23:27 -0300

    Add files via upload

---
+++ Livro - Módulo C#/2 - estrutura-sequencial/Exercicio.13/Program.cs
@@ -0,0 +1,24 @@
+﻿//13. Faca um algoritmo que:
+// •	Peca o valor a prazo do produto;
+// •	Peca a alíquota do desconto;
+// •	Calcule o preço a vista do produto.
+
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Informe o valor do produto em reais: ");
+        double valorProduto = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Informe o valor do desconto do produto em porcento: ");
+        double valorAliquota = Convert.ToDouble(Console.ReadLine());
+
+        double calculo = valorProduto - (valorProduto * valorAliquota / 100);
+
+        Console.WriteLine($"O produto de R$ {valorProduto:F2}, com o desconto de {valorAliquota}%, passa a custar R$ {calculo:F2} à vista.");
+    }
+}
+
+
+
---
+++ Livro - Módulo C#/2 - estrutura-sequencial/Exercicio.14/Program.cs
@@ -0,0 +1,25 @@
+﻿//14. Desenvolva um programa para cálculo de salário liquido de um funcionário. O programa deve:
+// •	Pedir o salário bruto do funcionário;
+// •	Calcular o valor do IR com alíquota de 10%;
+// •	Calcular o valor do INSS com alíquota de 5%;
+// •	Calcular o salário liquido do funcionário;
+
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Digite seu salário bruto em reais (R$): ");
+        double salarioBruto = Convert.ToDouble(Console.ReadLine());
+        double valorIR = salarioBruto * 0.10;
+        double valorINSS = salarioBruto * 0.05;
+
+        double valorLiquido = Math.Round(salarioBruto - valorIR - valorINSS, 2);
+
+        Console.WriteLine($"O salário líquido do funcionário é de R$ {valorLiquido:F2}.");
+    }
+}
+
+
+
