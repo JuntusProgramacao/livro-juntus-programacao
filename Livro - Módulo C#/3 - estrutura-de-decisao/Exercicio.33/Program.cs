46875b6b0e45c81b1a45d289d983595d02135525
Juntus Programação <144743726+JuntusProgramacao@users.noreply.github.com>
2024-04-08 08:49:21 -0300

    Add files via upload

---
+++ Livro - Módulo C#/3 - estrutura-de-decisao/Exercicio.33/Program.cs
@@ -0,0 +1,27 @@
+﻿//33. Faça um algoritmo que leia dois números, calcule e mostra a divisão do primeiro
+// número digitado pelo segundo. Lembre-se que não existe divisão por zero.
+using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Digite o primeiro número: ");
+        double num1 = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Digite o segundo número: ");
+        double num2 = Convert.ToDouble(Console.ReadLine());
+
+        double res = num1 / num2;
+
+        if (num1 == 0 || num2 == 0)
+        {
+            Console.WriteLine("A divisão por zero não é permitida.");
+        }
+        else
+        {
+            Console.WriteLine($"{num1} ÷ {num2} = {res}");
+        }
+    }
+}
+
+
---
+++ Livro - Módulo C#/3 - estrutura-de-decisao/Exercicio.34/Program.cs
@@ -0,0 +1,39 @@
+﻿//34. Faça um algoritmo que leia o código, o preço atual e o departamento de um produto,
+// calcule e mostre o preço novo, sabendo que para o departamento 1 o aumento foi de 5,0%
+// e para o departamento 2 o aumento foi de 7,4%.
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Digite o código do produto: ");
+        int codigo = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Digite o preço atual do produto: ");
+        double precoAtual = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Digite o departamento do produto (1 ou 2): ");
+        int departamento = Convert.ToInt32(Console.ReadLine());
+
+        double aumento;
+
+        if (departamento == 1)
+        {
+            aumento = precoAtual * 0.05;
+        }
+        else if (departamento == 2)
+        {
+            aumento = precoAtual * 0.074;
+        }
+        else
+        {
+            Console.WriteLine("Departamento inválido.");
+            return;
+        }
+
+        double precoNovo = precoAtual + aumento;
+
+        Console.WriteLine($"O preço novo do produto identificado com o código {codigo} é R$ {precoNovo:F2}");
+    }
+}
+
