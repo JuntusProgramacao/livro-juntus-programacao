3d0ca4ce5f3c990a4158dbbc1939e88244bd6cc6
Juntus Programação <144743726+JuntusProgramacao@users.noreply.github.com>
2024-04-08 12:13:28 -0300

    Add files via upload

---
+++ Livro - Módulo C#/5 - multipla-escolha/Exercicio.01/Program.cs
@@ -0,0 +1,65 @@
+﻿//1. Uma determinada universidade pretende desenvolver uma pesquisa através dos seus alunos matriculados. Para cada aluno será digitado um dos códigos 1,2 e 3 que significam:
+// • 1 indica que o aluno cursa Administração
+// • 2 indica que o aluno cursa Administração com gestão em Informática
+// • 3 indica que o aluno cursa Sistemas de Informação.
+// Deseja-se saber a porcentagem e o número de alunos por curso. Considere um número indeterminado de alunos matriculados.
+using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int totalAlunos = 0;
+        int alunosAdministracao = 0;
+        int alunosAdministracaoInformatica = 0;
+        int alunosSistemasInformacao = 0;
+
+        Console.WriteLine(
+            "Digite o código do curso do aluno (1 - Administração, 2 - Administração com gestão em Informática, 3 - Sistemas de Informação)"
+        );
+        Console.WriteLine("Digite 0 para encerrar o programa.");
+
+        int codigoCurso;
+        do
+        {
+            Console.Write("Código do curso do aluno: ");
+            codigoCurso = Convert.ToInt32(Console.ReadLine());
+
+            switch (codigoCurso)
+            {
+                case 1:
+                    alunosAdministracao++;
+                    break;
+                case 2:
+                    alunosAdministracaoInformatica++;
+                    break;
+                case 3:
+                    alunosSistemasInformacao++;
+                    break;
+                case 0:
+                    break;
+                default:
+                    Console.WriteLine("Código inválido. Tente novamente.");
+                    continue;
+            }
+            totalAlunos++;
+        } while (codigoCurso != 0);
+
+        double porcentagemAdministracao = (double)alunosAdministracao / totalAlunos * 100;
+        double porcentagemAdministracaoInformatica =
+            (double)alunosAdministracaoInformatica / totalAlunos * 100;
+        double porcentagemSistemasInformacao = (double)alunosSistemasInformacao / totalAlunos * 100;
+
+        Console.WriteLine("===== RESULTADOS =====");
+        Console.WriteLine(
+            $"Porcentagem de alunos em Administração: {porcentagemAdministracao}% ({alunosAdministracao})"
+        );
+        Console.WriteLine(
+            $"Porcentagem de alunos em Administração com gestão em Informática: {porcentagemAdministracaoInformatica}% ({alunosAdministracaoInformatica})"
+        );
+        Console.WriteLine(
+            $"Porcentagem de alunos em Sistemas de Informação: {porcentagemSistemasInformacao}% ({alunosSistemasInformacao})"
+        );
+        Console.ReadLine();
+    }
+}
+
---
+++ Livro - Módulo C#/5 - multipla-escolha/Exercicio.02/Program.cs
@@ -0,0 +1,101 @@
+﻿//2. Para eleição de representantes de classe de uma universidade há três candidatos. Os votos são informados através de código: 1, 2 ou 3, voto para os
+// respectivos candidatos, 5 voto nulo e 6 voto em branco. Faça um algoritmo que calcule e escreva:
+// a. A porcentagem e o total de votos para cada candidato
+// b. Total de votos nulos
+// c. Total de votos em branco
+// d. Percentual de votos em brancos e nulos
+// e. Classificação dos candidatos
+// f. Total de votos
+
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int candidato1 = 0;
+        int candidato2 = 0;
+        int candidato3 = 0;
+        int votosNulos = 0;
+        int votosBrancos = 0;
+        int totalVotos = 0;
+        int voto;
+
+        do
+        {
+            Console.WriteLine("Informe seu voto:\n1, 2 ou 3 para os candidatos \n5 para voto nulo \n6 para voto em branco\n0 para encerrar o programa");
+            voto = int.Parse(Console.ReadLine());
+            switch (voto)
+            {
+                case 1:
+                    candidato1++;
+                    totalVotos++;
+                    break;
+                case 2:
+                    candidato2++;
+                    totalVotos++;
+                    break;
+                case 3:
+                    candidato3++;
+                    totalVotos++;
+                    break;
+                case 5:
+                    votosNulos++;
+                    totalVotos++;
+                    break;
+                case 6:
+                    votosBrancos++;
+                    totalVotos++;
+                    break;
+                default:
+                    Console.WriteLine("Voto inválido. Tente novamente.");
+                    break;
+            }
+        } while (voto != 0);
+        
+        Console.WriteLine("");
+        Console.WriteLine("RESULTADO DAS ELEIÇÕES");
+        Console.WriteLine("");
+
+        double percentualCandidato1 = (double)candidato1 / totalVotos * 100;
+        double percentualCandidato2 = (double)candidato2 / totalVotos * 100;
+        double percentualCandidato3 = (double)candidato3 / totalVotos * 100;
+
+        Console.WriteLine("------------------------------------");
+        Console.WriteLine("\nCandidato 1: {0} votos ({1}% do total)", candidato1, percentualCandidato1.ToString("F2"));
+        Console.WriteLine("Candidato 2: {0} votos ({1}% do total)", candidato2, percentualCandidato2.ToString("F2"));
+        Console.WriteLine("Candidato 3: {0} votos ({1}% do total)", candidato3, percentualCandidato3.ToString("F2"));
+        Console.WriteLine("Votos Nulos: {0}", votosNulos);
+        Console.WriteLine("Votos em Branco: {0}", votosBrancos);
+
+        double percentualNulos = (double)votosNulos / totalVotos * 100;
+        double percentualBrancos = (double)votosBrancos / totalVotos * 100;
+
+        Console.WriteLine("Percentual de votos nulos: {0}%", percentualNulos.ToString("F2"));
+        Console.WriteLine("Percentual de votos em branco: {0}%\n", percentualBrancos.ToString("F2"));
+
+        Console.WriteLine("------------------------------------");
+
+        if (candidato1 > candidato2 && candidato1 > candidato3)
+        {
+            Console.WriteLine("VENCEDOR: CANDIDATO 1");
+        }
+        else if (candidato2 > candidato1 && candidato2 > candidato3)
+        {
+            Console.WriteLine("VENCEDOR: CANDIDATO 1");
+        }
+        else if (candidato3 > candidato1 && candidato3 > candidato2)
+        {
+            Console.WriteLine("VENCEDOR: CANDIDATO 1");
+        }
+        else
+        {
+            Console.WriteLine("A eleição terminou em empate.");
+        }
+
+        Console.WriteLine("------------------------------------");
+        Console.WriteLine("\nTotal de votos: {0}", totalVotos);
+    }
+}
+
+
