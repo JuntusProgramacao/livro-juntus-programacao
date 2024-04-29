d119e5e66f1cbcba654d1d3cf06e0fc0e293bdcc
Juntus Programação <144743726+JuntusProgramacao@users.noreply.github.com>
2024-04-29 10:27:13 -0300

    Delete Módulo Python/Códigos directory

--- Módulo Python/Códigos/2 - Estrutura Sequencial/Exercicio.04.py
+++
@@ -1,8 +0,0 @@
-#4. Faça um algoritmo para ler três números inteiros e calcule a multiplicação entre eles.
-
-n1 = int(input('Digite número 1: '))
-n2 = int(input('Digite número 2: '))
-n3 = int(input('Digite número 3: '))
-
-print(f'O resultado da multiplicação é de: {n1*n2*n3}')
-
--- Módulo Python/Códigos/2 - Estrutura Sequencial/Exercicio.22.py
+++
@@ -1,9 +0,0 @@
-#22. Durante uma viagem a Inglaterra você foi obrigado a comprar um termômetro, 
-# e verificou que este estava marcando a temperatura em Fahrenheit. Faça um algoritmo 
-# para calcular a conversão de graus Celcius para Fahrenheit.
-
-celsius = float(input("Digite a temperatura em graus Celsius: "))
-fahrenheit = (celsius * 9/5) + 32
-print("A temperatura em graus Fahrenheit é:", fahrenheit)
-
-
